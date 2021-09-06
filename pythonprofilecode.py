42dbea7d18b24611ac141ecca4668eaa03eef4cb
warming151 <64031030+warming151@users.noreply.github.com>
2021-09-05 23:04:53 -0400

    Add files via upload

---
+++ pythonprofilecode.py
@@ -0,0 +1,20 @@
+import cProfile
+
+# Code containing multiple dunctions
+def create_array():
+  arr=[]
+  for i in range(0,5000):
+  	print(i)
+  	arr.append(i)
+
+def print_statement():
+  print('Array created successfully')
+
+
+def main():
+  create_array()
+  print_statement()
+
+
+if __name__ == '__main__':
+    cProfile.run('main()')
